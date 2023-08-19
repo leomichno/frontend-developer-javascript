6db99646a42ff3ddab55a20bd1d1de1fab790d42
Leonardo <leo_michno@hotmial.com>
2023-08-19 11:16:41 -0300

    agregamos todos los productos de forma dimanica desde javascript

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
 const commandMenuActivator=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
-
+const cardsConteiner=document.querySelector('.cards-container')
 commandMenuActivator.addEventListener('click', activator);
 
 
 function activator(){
     desktopMenu.classList.toggle('inactivity')
 }   
+
+const productos=[];
+productos.push({img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+precio:"120",
+titulo:"bike",
+imgcard:"./icons/bt_add_to_cart.svg",
+});
+productos.push({img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+precio:"120",
+titulo:"bike",
+imgcard:"./icons/bt_add_to_cart.svg",
+});
+productos.push({img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+precio:"120",
+titulo:"bike",
+imgcard:"./icons/bt_add_to_cart.svg",
+});
+productos.push({img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+precio:"120",
+titulo:"bike",
+imgcard:"./icons/bt_add_to_cart.svg",
+});
+
+for ( product of productos ){
+   const productCard=document.createElement('div');
+   productCard.classList.add('product-card');
+   
+   const productImg=document.createElement('img');
+   productImg.setAttribute('src',product.img);
+
+   const productInfo=document.createElement('div');
+   productInfo.classList.add('product-info');
+
+
+
+   const productInfoDiv=document.createElement('div');
+
+   const productPrice=document.createElement('p');
+   productPrice.innerText= '$' + product.precio;
+   const productName=document.createElement('p');
+   productName.innerText = product.titulo;
+
+   productInfoDiv.appendChild(productPrice);
+   productInfoDiv.appendChild(productName);
+
+   const productInfoFigure=document.createElement('figure');
+   const productImgCart = document.createElement('img');
+   productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+   productInfoFigure.appendChild(productImgCart);
+
+
+   productInfo.appendChild(productInfoDiv);
+   productInfo.appendChild(productInfoFigure);
+
+   productCard.appendChild(productImg);
+   productCard.appendChild(productInfo);
+
+   cardsConteiner.appendChild(productCard);
+}
